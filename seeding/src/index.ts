92e0c8939f4ee319acfa19913b3a551e77c64afd
heart <>
2022-06-21 00:45:26 +0200

    evaluator first pass

--- seeding/src/index.ts
+++ seeding/src/index.ts
@@ -1,36 +1,42 @@
 import fs from "fs";
 import { getAreas, getGems, getQuests } from "./seeding";
-import { parseRoute } from "./route";
+import { InitializeRouteState, parseRoute } from "./route";
 
-const persistPath = process.argv[2];
-function persistData(name: string, data: any) {
-  fs.writeFileSync(
-    `${persistPath}/${name}.json`,
-    JSON.stringify(data, null, 2)
-  );
+const dataPath = process.argv[2];
+function saveData(name: string, data: any) {
+  fs.writeFileSync(`${dataPath}/${name}.json`, JSON.stringify(data, null, 2));
+}
+function loadData(name: string) {
+  return JSON.parse(fs.readFileSync(`${dataPath}/${name}.json`, "utf-8"));
 }
 
 export async function main() {
-  if (!fs.existsSync(persistPath))
-    fs.mkdirSync(persistPath, { recursive: true });
+  if (!fs.existsSync(dataPath)) fs.mkdirSync(dataPath, { recursive: true });
 
   const command = process.argv[3];
   switch (command) {
     case "seed_data":
       {
         const gems = await getGems();
-        persistData("gems", gems);
+        saveData("gems", gems);
 
         const quests = await getQuests();
-        persistData("quests", quests);
+        saveData("quests", quests);
 
         const areas = await getAreas();
-        persistData("areas", areas);
+        saveData("areas", areas);
       }
       break;
     case "parse_route":
       {
-        await parseRoute(`${persistPath}/route.txt`);
+        const quests = loadData("quests");
+        const areas = loadData("areas");
+        const bossWaypoints = loadData("boss-waypoints");
+
+        await parseRoute(
+          `${dataPath}/route.txt`,
+          InitializeRouteState(quests, areas, bossWaypoints)
+        );
       }
       break;
     default:
--- seeding/src/route.ts
+++ seeding/src/route.ts
@@ -1,8 +1,42 @@
 import fs from "fs";
 import readline from "readline";
+import { Area, Monster, Quest } from "../../types";
 
 type Action = string[];
 type Step = (string | Action)[];
+
+export function InitializeRouteState(
+  quests: Record<string, Quest>,
+  areas: Record<string, Area>,
+  bossWaypoints: Record<Monster["name"], Area["id"]>
+): RouteState {
+  const state: RouteState = {
+    quests: quests,
+    areas: areas,
+    towns: {},
+    bossWaypoints: bossWaypoints,
+    waypoints: new Set(),
+    currentAreaId: "1_1_1",
+    portalAreaId: null,
+  };
+
+  for (const id in areas) {
+    const area = areas[id];
+    if (area.is_town_area) state.towns[area.act] = area.id;
+  }
+
+  return state;
+}
+
+export interface RouteState {
+  quests: Record<string, Quest>;
+  areas: Record<string, Area>;
+  towns: Record<Area["act"], Area["id"]>;
+  bossWaypoints: Record<Monster["name"], Area["id"]>;
+  waypoints: Set<Area["id"]>;
+  currentAreaId: Area["id"];
+  portalAreaId: Area["id"] | null;
+}
 
 function parseStep(text: string) {
   const regex = /(\s*#.*)|([^{]+)|\{(.+?)\}/g;
@@ -29,38 +63,116 @@ function parseStep(text: string) {
   return steps;
 }
 
-const actionValidators: Record<string, (action: Action) => boolean> = {
-  kill: (action: Action) => action.length == 2,
-  enter: (action: Action) => action.length == 2,
-  town: (action: Action) => action.length == 1,
-  waypoint: (action: Action) => action.length == 2,
-  get_waypoint: (action: Action) => action.length == 1,
-  set_portal: (action: Action) => action.length == 1,
-  use_portal: (action: Action) => action.length == 1,
-  area: (action: Action) => action.length == 2,
-  quest: (action: Action) => action.length == 2,
-  quest_item: (action: Action) => action.length == 2,
-  vendor: (action: Action) => action.length == 1,
-  trial: (action: Action) => action.length == 1,
+type ActionEvaluator = (action: Action, state: RouteState) => boolean;
+const actionEvaluators: Record<string, ActionEvaluator> = {
+  kill: (action, state) => {
+    if (action.length != 2) return false;
+    const bossName = action[1];
+
+    const currentArea = state.areas[state.currentAreaId];
+    if (!currentArea.bosses.some((x) => x.name == bossName)) return false;
+
+    const waypointUnlocks = state.bossWaypoints[bossName];
+    if (waypointUnlocks) {
+      for (const waypointUnlock of waypointUnlocks) {
+        state.waypoints.add(waypointUnlock);
+      }
+    }
+
+    return true;
+  },
+  area: (action, state) =>
+    action.length == 2 && state.areas[action[1]] !== undefined,
+  enter: (action, state) => {
+    if (action.length != 2) return false;
+
+    const area = state.areas[action[1]];
+    if (!area) return false;
+    if (!area.connection_ids.some((x) => x == state.currentAreaId))
+      return false;
+
+    if (area.is_town_area) state.waypoints.add(area.id);
+    state.currentAreaId = area.id;
+    return true;
+  },
+  town: (action, state) => {
+    if (action.length != 1) return false;
+
+    const area = state.areas[state.currentAreaId];
+    if (!area) return false;
+
+    state.currentAreaId = state.towns[area.act];
+    return true;
+  },
+  waypoint: (action, state) => {
+    if (action.length != 2) return false;
+
+    const area = state.areas[action[1]];
+    if (!area) return false;
+    if (!state.waypoints.has(area.id)) return false;
+
+    state.currentAreaId = area.id;
+    return true;
+  },
+  get_waypoint: (action, state) => {
+    if (action.length != 1) return false;
+
+    const area = state.areas[state.currentAreaId];
+    if (!area) return false;
+    if (state.waypoints.has(area.id)) return false;
+    if (!area.has_waypoint) return false;
+
+    state.waypoints.add(area.id);
+    return true;
+  },
+  set_portal: (action, state) => {
+    if (action.length != 1) return false;
+
+    state.portalAreaId = state.currentAreaId;
+    return true;
+  },
+  use_portal: (action, state) => {
+    if (action.length != 1) return false;
+    if (!state.portalAreaId) return false;
+
+    const currentArea = state.areas[state.currentAreaId];
+    if (currentArea.id == state.portalAreaId) {
+      const townId = state.towns[currentArea.act];
+      state.portalAreaId = state.currentAreaId;
+      state.currentAreaId = townId;
+    } else {
+      if (!currentArea.is_town_area) return false;
+      state.currentAreaId = state.portalAreaId;
+      state.portalAreaId = null;
+    }
+
+    return true;
+  },
+  quest: (action, state) =>
+    action.length == 2 && state.quests[action[1]] !== undefined,
+  quest_item: (action, state) => action.length == 2,
+  vendor: (action, state) => action.length == 1,
+  trial: (action, state) => action.length == 1,
 };
 
-function validateSteps(steps: Step[]) {
-  for (const step of steps) {
-    for (const subStep of step) {
-      if (typeof subStep == "string") continue;
-      if (subStep.length == 0) throw new Error(subStep.toString());
+function validateStep(step: Step, state: RouteState) {
+  for (const subStep of step) {
+    if (typeof subStep == "string") continue;
+    if (subStep.length == 0) throw new Error(subStep.toString());
 
-      const actionKey = subStep[0];
-      const validator = actionValidators[actionKey];
-      if (!validator) throw new Error(subStep.toString());
-
-      const isValid = validator(subStep);
-      if (!isValid) throw new Error(subStep.toString());
+    const actionKey = subStep[0];
+    const evaluator = actionEvaluators[actionKey];
+    if (!evaluator) throw new Error(subStep.toString());
+    try {
+      const success = evaluator(subStep, state);
+      if (!success) console.log(subStep.toString());
+    } catch (e) {
+      console.log(e);
     }
   }
 }
 
-export async function parseRoute(routePath: string) {
+export async function parseRoute(routePath: string, state: RouteState) {
   const fileStream = fs.createReadStream(routePath);
 
   const rl = readline.createInterface({
@@ -72,11 +184,8 @@ export async function parseRoute(routePath: string) {
   for await (const line of rl) {
     if (!line) continue;
 
-    let step = parseStep(line);
+    const step = parseStep(line);
+    validateStep(step, state);
     if (step.length > 0) steps.push(step);
   }
-
-  validateSteps(steps);
-
-  console.log(steps);
 }
