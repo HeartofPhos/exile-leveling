fe50b54aa5cc338161eb966e0f3ca59bc5d45153
heart <>
2022-06-22 03:32:20 +0200

    avoid multi word expansions

--- web/src/components/Action/index.tsx
+++ web/src/components/Action/index.tsx
@@ -21,52 +21,29 @@ const directions = [
 type ActionMapper = (action: Action, state: RouteState) => JSX.Element;
 const ACTION_MAPPER_LOOKUP: Record<string, ActionMapper> = {
   kill: (action, state) => (
-    <>
-      <span>Kill </span>
-      <span className={classNames("enemy")}>{action[1]}</span>
-    </>
+    <span className={classNames("enemy")}>{action[1]}</span>
   ),
   area: (action, state) => (
-    <>
-      <span className={classNames("area")}>{state.areas[action[1]].name}</span>
-    </>
+    <span className={classNames("area")}>{state.areas[action[1]].name}</span>
   ),
   enter: (action, state) => (
-    <>
-      <span>Enter </span>
-      <span className={classNames("area")}>{state.areas[action[1]].name}</span>
-    </>
+    <span className={classNames("area")}>{state.areas[action[1]].name}</span>
   ),
   quest: (action, state) => {
     const quest = state.quests[action[1]];
     return (
-      <>
-        <span>Hand in </span>
-        <span className={classNames("quest")}>{quest?.quest || action[1]}</span>
-      </>
+      <span className={classNames("quest")}>{quest?.quest || action[1]}</span>
     );
   },
   quest_item: (action, state) => {
-    return (
-      <>
-        <span className={classNames("quest_item")}>{action[1]}</span>
-      </>
-    );
+    return <span className={classNames("quest_item")}>{action[1]}</span>;
   },
   quest_text: (action, state) => {
-    return (
-      <>
-        <span className={classNames("quest_text")}>{action[1]}</span>
-      </>
-    );
+    return <span className={classNames("quest_text")}>{action[1]}</span>;
   },
   waypoint: (action, state) => {
     if (action.length == 1)
-      return (
-        <>
-          <span className={classNames("waypoint")}>Waypoint</span>
-        </>
-      );
+      return <span className={classNames("waypoint")}>Waypoint</span>;
 
     return (
       <>
@@ -79,12 +56,7 @@ const ACTION_MAPPER_LOOKUP: Record<string, ActionMapper> = {
     );
   },
   get_waypoint: (action, state) => {
-    return (
-      <>
-        <span>Get </span>
-        <span className={classNames("waypoint")}>Waypoint</span>
-      </>
-    );
+    return <span className={classNames("waypoint")}>Waypoint</span>;
   },
   vendor: (action, state) => {
     return (
@@ -95,12 +67,7 @@ const ACTION_MAPPER_LOOKUP: Record<string, ActionMapper> = {
     );
   },
   trial: (action, state) => {
-    return (
-      <>
-        <span>Complete </span>
-        <span className={classNames("trial")}>Trial of Ascendancy</span>
-      </>
-    );
+    return <span className={classNames("trial")}>Trial of Ascendancy</span>;
   },
   town: (action, state) => {
     return (
@@ -111,44 +78,20 @@ const ACTION_MAPPER_LOOKUP: Record<string, ActionMapper> = {
     );
   },
   set_portal: (action, state) => {
-    return (
-      <>
-        <span>Place </span>
-        <span className={classNames("portal")}>Portal</span>
-      </>
-    );
+    return <span className={classNames("portal")}>Portal</span>;
   },
   use_portal: (action, state) => {
-    return (
-      <>
-        <span>Take </span>
-        <span className={classNames("portal")}>Portal</span>
-      </>
-    );
+    return <span className={classNames("portal")}>Portal</span>;
   },
   dir: (action, state) => {
     let dirIndex = Math.floor(Number.parseFloat(action[1]) / 45);
-    return (
-      <>
-        <span>{directions[dirIndex]}</span>
-      </>
-    );
+    return <span>{directions[dirIndex]}</span>;
   },
-  talk: (action, state) => {
-    return (
-      <>
-        <span>Talk to </span>
-        <span className={classNames("talk")}>{action[1]}</span>
-      </>
-    );
+  npc: (action, state) => {
+    return <span className={classNames("npc")}>{action[1]}</span>;
   },
   crafting: (action, state) => {
-    return (
-      <>
-        <span>Get </span>
-        <span className={classNames("crafting")}>Crafting Recipe</span>
-      </>
-    );
+    return <span className={classNames("crafting")}>Crafting Recipe</span>;
   },
 };
 
