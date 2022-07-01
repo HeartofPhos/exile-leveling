f33c4bbc86e766a7a649bef33127c92b330546ab
heart <>
2022-07-01 08:53:44 +0200

    move gem css

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Gems } from "./containers/Gems";
 import RoutesContainer from "./containers/Routes";
+import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
