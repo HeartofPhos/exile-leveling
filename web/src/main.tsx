ad983bace54f8a5f7a46e9c705d39b4f9a2bc744
heart <>
2022-07-02 17:20:19 +0200

    hashrouter

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Gems } from "./containers/Gems";
 import RoutesContainer from "./containers/Routes";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<RoutesContainer />} />
         <Route path="/gems" element={<Gems />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
