4b0b30d5e314fbe1c8fc09d7f1d74e140601b0d8
heart <>
2022-07-02 16:31:58 +0200

    move data

--- web/src/containers/Routes/index.tsx
+++ web/src/containers/Routes/index.tsx
@@ -9,12 +9,23 @@ import {
 import { ExileList } from "../../components/ExileList";
 import { ExileStep } from "../../components/ExileStep";
 
+function getDataUrl(path: string) {
+  return new URL(`/src/data/${path}`, import.meta.url).href;
+}
+
 const ROUTE_PATHS = [
-  `${import.meta.env.BASE_URL}/routes/act-1.txt`,
-  `${import.meta.env.BASE_URL}/routes/act-2.txt`,
-  `${import.meta.env.BASE_URL}/routes/act-3.txt`,
-  `${import.meta.env.BASE_URL}/routes/act-4.txt`,
-  `${import.meta.env.BASE_URL}/routes/act-5.txt`,
+  getDataUrl(`routes/act-1.txt`),
+  getDataUrl(`routes/act-2.txt`),
+  getDataUrl(`routes/act-3.txt`),
+  getDataUrl(`routes/act-4.txt`),
+  getDataUrl(`routes/act-5.txt`),
+];
+
+const DATA_PATHS = [
+  getDataUrl(`quests.json`),
+  getDataUrl(`areas.json`),
+  getDataUrl(`boss-waypoints.json`),
+  getDataUrl(`gems.json`),
 ];
 
 interface RouteData {
@@ -27,21 +38,9 @@ function RoutesContainer() {
 
   useEffect(() => {
     const fn = async () => {
-      const quests = await fetch(
-        `${import.meta.env.BASE_URL}/data/quests.json`
-      ).then((x) => x.json());
-
-      const areas = await fetch(
-        `${import.meta.env.BASE_URL}/data/areas.json`
-      ).then((x) => x.json());
-
-      const bossWaypoints = await fetch(
-        `${import.meta.env.BASE_URL}/data/boss-waypoints.json`
-      ).then((x) => x.json());
-
-      const gems = await fetch(
-        `${import.meta.env.BASE_URL}/data/gems.json`
-      ).then((x) => x.json());
+      const [quests, areas, bossWaypoints, gems] = await Promise.all(
+        DATA_PATHS.map((x) => fetch(x).then((x) => x.json()))
+      );
 
       const routeLookup = initializeRouteLookup(
         quests,
@@ -51,14 +50,13 @@ function RoutesContainer() {
       );
       const routeState = initializeRouteState();
 
-      let routes: Route[] = [];
-      for (const path of ROUTE_PATHS) {
-        const route = await fetch(path)
-          .then((x) => x.text())
-          .then((x) => parseRoute(x, routeLookup, routeState));
-
-        routes.push(route);
-      }
+      const routes: Route[] = await Promise.all(
+        ROUTE_PATHS.map((x) =>
+          fetch(x)
+            .then((x) => x.text())
+            .then((x) => parseRoute(x, routeLookup, routeState))
+        )
+      );
 
       setRouteData({ routes: routes, lookup: routeLookup });
     };
