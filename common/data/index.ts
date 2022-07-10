0db4782de8199fac8bc2f946039b5ba32ba06a13
heart <>
2022-07-10 03:41:54 +0200

    sanity

--- common/data/index.ts
+++ common/data/index.ts
@@ -1,16 +1,16 @@
 import { Quests, Areas, BossWaypoints, Gems, GemColours } from "../types";
 
-import questsJson from "./quests.json";
-import areasJson from "./areas.json";
-import bossWaypointsJson from "./boss-waypoints.json";
-import gemsJson from "./gems.json";
-import gemColoursJson from "./gem-colours.json";
+import QUESTS_JSON from "./json/quests.json";
+import AREAS_JSON from "./json/areas.json";
+import BOSS_WAYPOINTS_JSON from "./json/boss-waypoints.json";
+import GEMS_JSON from "./json/gems.json";
+import GEM_COLOURS_JSON from "./json/gem-colours.json";
 
-export const quests = questsJson as Quests;
-export const areas = areasJson as Areas;
-export const bossWaypoints = bossWaypointsJson as BossWaypoints;
-export const gems = gemsJson as Gems;
-export const gemColours = gemColoursJson as GemColours;
+export const quests = QUESTS_JSON as Quests;
+export const areas = AREAS_JSON as Areas;
+export const bossWaypoints = BOSS_WAYPOINTS_JSON as BossWaypoints;
+export const gems = GEMS_JSON as Gems;
+export const gemColours = GEM_COLOURS_JSON as GemColours;
 
 //@ts-expect-error
 export const routeFiles: Record<string, string> = import.meta.glob(
