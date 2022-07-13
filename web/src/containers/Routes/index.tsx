410acfa17ec91bff97f266202f3115f4880918bb
heart <>
2022-07-13 02:28:02 +0200

    restructure routes container

--- web/src/containers/Routes/index.tsx
+++ web/src/containers/Routes/index.tsx
@@ -13,68 +13,60 @@ import React from "react";
 import { routeFiles } from "../../../../common/data";
 import { getPersistent, setPersistent } from "../../utility";
 
-interface RouteData {
-  routes: Route[];
-  lookup: RouteLookup;
-}
-
-interface RoutesContainerState {
-  routeData: RouteData;
-}
-
 type RouteProgress = boolean[][];
 
-export class RoutesContainer extends React.Component<{}, RoutesContainerState> {
+export class RoutesContainer extends React.Component {
+  private routes: Route[];
+  private routeLookup: RouteLookup;
   private routeProgress: RouteProgress;
 
   constructor(props: {}) {
     super(props);
 
     const buildData = getPersistent<BuildData>("build-data");
-    const routeLookup = initializeRouteLookup(buildData);
+    this.routeLookup = initializeRouteLookup(buildData);
     const routeState = initializeRouteState();
-
-    const routes = parseRoute(routeFiles, routeLookup, routeState);
-
-    this.state = {
-      routeData: { routes: routes, lookup: routeLookup },
-    };
+    this.routes = parseRoute(routeFiles, this.routeLookup, routeState);
 
     const routeProgress = getPersistent<RouteProgress>("route-progress");
     if (routeProgress) {
       this.routeProgress = routeProgress;
     } else {
       this.routeProgress = [];
-      for (const route of routes) {
+      for (const route of this.routes) {
         this.routeProgress.push(route.map(() => false));
       }
     }
   }
 
   render(): ReactNode {
-    return (
-      <>
-        {this.state.routeData.routes.map((route, routeIndex) => (
-          <ExileList
-            key={routeIndex}
-            header={`--== Act ${routeIndex + 1} ==--`}
-            initialCompleted={this.routeProgress[routeIndex]}
-            onUpdate={(index, isCompleted) => {
-              this.routeProgress[routeIndex][index] = isCompleted;
-              setPersistent("route-progress", this.routeProgress);
-            }}
-          >
-            {route.map((step, stepIndex) => (
-              <ExileStep
-                key={stepIndex}
-                step={step}
-                lookup={this.state.routeData.lookup}
-              />
-            ))}
-          </ExileList>
-        ))}
-      </>
-    );
+    const items: ReactNode[] = [];
+    for (let routeIndex = 0; routeIndex < this.routes.length; routeIndex++) {
+      const route = this.routes[routeIndex];
+
+      let steps: ReactNode[] = [];
+      for (let stepIndex = 0; stepIndex < route.length; stepIndex++) {
+        const step = route[stepIndex];
+        steps.push(
+          <ExileStep key={stepIndex} step={step} lookup={this.routeLookup} />
+        );
+      }
+
+      items.push(
+        <ExileList
+          key={routeIndex}
+          header={`--== Act ${routeIndex + 1} ==--`}
+          initialCompleted={this.routeProgress[routeIndex]}
+          onUpdate={(index, isCompleted) => {
+            this.routeProgress[routeIndex][index] = isCompleted;
+            setPersistent("route-progress", this.routeProgress);
+          }}
+        >
+          {steps}
+        </ExileList>
+      );
+    }
+    return <>{items}</>;
   }
 }
 export default RoutesContainer;
