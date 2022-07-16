7e2c7d9ecce7a2fc5d58f30efb55ffe152b634d2
heart <>
2022-07-16 04:42:04 +0200

    sanity

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -10,10 +10,12 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HashRouter>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<RoutesContainer />} />
-        <Route path="/build" element={<Build />} />
-      </Routes>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<RoutesContainer />} />
+          <Route path="/build" element={<Build />} />
+        </Routes>
+      </div>
     </HashRouter>
   </React.StrictMode>
 );
