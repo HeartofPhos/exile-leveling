ccdbfc7908175f002ffc10288d0a7c4192e2e9fc
heart <>
2022-08-03 22:00:02 +0200

    refactor mouseDown dragging move collapsing header to dedicated component fix spacing/quotes

--- web/src/components/TaskList/index.tsx
+++ web/src/components/TaskList/index.tsx
@@ -1,88 +1,48 @@
-import classNames from 'classnames';
-import { RecoilState, useRecoilState } from 'recoil';
-import { useState } from 'react';
-import styles from './TaskList.module.css';
+import classNames from "classnames";
+import { RecoilState, useRecoilState } from "recoil";
+import styles from "./TaskList.module.css";
 
 export const taskStyle = styles.task;
 
 export interface TaskItemProps {
-	key?: any;
-	children?: React.ReactNode;
-	isCompletedState?: RecoilState<boolean>;
-	mouseDown?: boolean;
-	setMouseDown?: React.Dispatch<React.SetStateAction<boolean>>;
+  key?: any;
+  children?: React.ReactNode;
+  isCompletedState?: RecoilState<boolean>;
 }
 
-function TaskListItem({
-	children,
-	isCompletedState,
-	mouseDown,
-}: TaskItemProps) {
-	const [isCompleted, setIsCompleted] = isCompletedState
-		? useRecoilState(isCompletedState)
-		: [undefined, undefined];
+function TaskListItem({ children, isCompletedState }: TaskItemProps) {
+  const [isCompleted, setIsCompleted] = isCompletedState
+    ? useRecoilState(isCompletedState)
+    : [undefined, undefined];
 
-	return (
-		<li
-			onMouseDown={() => {
-				if (setIsCompleted) setIsCompleted(!isCompleted);
-			}}
-			onMouseEnter={() => {
-				if (mouseDown) {
-					if (setIsCompleted) setIsCompleted(!isCompleted);
-				}
-			}}
-			className={classNames({ [styles.completed]: isCompleted })}
-		>
-			{children}
-		</li>
-	);
+  return (
+    <li
+      onMouseDown={(e) => {
+        if ((e.buttons & 1) == 0) return;
+        if (setIsCompleted) setIsCompleted(!isCompleted);
+      }}
+      onMouseEnter={(e) => {
+        if ((e.buttons & 1) == 0) return;
+        if (setIsCompleted) setIsCompleted(!isCompleted);
+      }}
+      className={classNames({ [styles.completed]: isCompleted })}
+    >
+      {children}
+    </li>
+  );
 }
 
 interface TaskListProps {
-	items?: TaskItemProps[];
-	act: number;
+  items?: TaskItemProps[];
 }
 
-export function TaskList({ items, act }: TaskListProps) {
-	const collapseSection = () => {
-		setCollapsed(!collapsed);
-	};
-	const [collapsed, setCollapsed] = useState(false);
-	const [mouseDown, setMouseDown] = useState(false);
-	if (items) {
-		return (
-			<div
-				onMouseDown={() => setMouseDown(true)}
-				onMouseUp={() => setMouseDown(false)}
-			>
-				<div
-					id={`act-${act}`}
-					key={items.length}
-					className='header'
-					onClick={() => collapseSection()}
-				>{`--== Act ${act} ==--`}</div>
-				{!collapsed ? (
-					<>
-						<hr key={items.length + 1} />
-						<ol className={classNames(styles.list)}>
-							{items &&
-								items.map(({ key, ...rest }, i) => (
-									<TaskListItem
-										key={key || i}
-										{...rest}
-										mouseDown={mouseDown}
-										setMouseDown={setMouseDown}
-									/>
-								))}
-						</ol>
-					</>
-				) : (
-					<></>
-				)}
-				<hr key={items.length + 3} />
-				<div />
-			</div>
-		);
-	} else return <></>;
+export function TaskList({ items }: TaskListProps) {
+  return (
+    <ol className={classNames(styles.list)}>
+      {items &&
+        items.map(({ key, ...rest }, i) => (
+          <TaskListItem key={key || i} {...rest} />
+        ))}
+    </ol>
+  );
 }
--- web/src/containers/Routes/index.tsx
+++ web/src/containers/Routes/index.tsx
@@ -1,86 +1,112 @@
-import { ReactNode } from 'react';
-import { TaskList, TaskItemProps } from '../../components/TaskList';
-import { ExileStep } from '../../components/ExileStep';
+import { ReactNode, useState } from "react";
+import { TaskList, TaskItemProps } from "../../components/TaskList";
+import { ExileStep } from "../../components/ExileStep";
 import {
-	gemProgressAtomFamily,
-	routeProgressAtomFamily,
-	routeDataAtom,
-	buildDataAtom,
-} from '../../utility/ExileSyncStore';
-import { findGems } from '../../../../common/routes/quest';
-import { GemReward } from '../../components/GemReward';
-import { withScrollRestoration } from '../../utility/withScrollRestoration';
-import { useRecoilValue } from 'recoil';
+  gemProgressAtomFamily,
+  routeProgressAtomFamily,
+  routeDataAtom,
+  buildDataAtom,
+} from "../../utility/ExileSyncStore";
+import { findGems } from "../../../../common/routes/quest";
+import { GemReward } from "../../components/GemReward";
+import { withScrollRestoration } from "../../utility/withScrollRestoration";
+import { useRecoilValue } from "recoil";
 
 function RoutesContainer() {
-	const { routes, routeLookup } = useRecoilValue(routeDataAtom);
-	const buildData = useRecoilValue(buildDataAtom);
+  const { routes, routeLookup } = useRecoilValue(routeDataAtom);
+  const buildData = useRecoilValue(buildDataAtom);
 
-	const routeGems: Set<number> = new Set();
-	const items: ReactNode[] = [];
-	for (let routeIndex = 0; routeIndex < routes.length; routeIndex++) {
-		const route = routes[routeIndex];
+  const routeGems: Set<number> = new Set();
+  const items: ReactNode[] = [];
+  for (let routeIndex = 0; routeIndex < routes.length; routeIndex++) {
+    const route = routes[routeIndex];
 
-		let taskItems: TaskItemProps[] = [];
-		for (let stepIndex = 0; stepIndex < route.length; stepIndex++) {
-			const step = route[stepIndex];
-			taskItems.push({
-				key: stepIndex,
-				isCompletedState: routeProgressAtomFamily([routeIndex, stepIndex]),
-				children: (
-					<ExileStep key={stepIndex} step={step} lookup={routeLookup} />
-				),
-			});
+    let taskItems: TaskItemProps[] = [];
+    for (let stepIndex = 0; stepIndex < route.length; stepIndex++) {
+      const step = route[stepIndex];
+      taskItems.push({
+        key: stepIndex,
+        isCompletedState: routeProgressAtomFamily([routeIndex, stepIndex]),
+        children: (
+          <ExileStep key={stepIndex} step={step} lookup={routeLookup} />
+        ),
+      });
 
-			if (buildData) {
-				if (step.type == 'fragment_step') {
-					for (const part of step.parts) {
-						if (typeof part !== 'string' && part.type == 'quest') {
-							const { questGems, vendorGems } = findGems(
-								part,
-								buildData,
-								routeGems
-							);
+      if (buildData) {
+        if (step.type == "fragment_step") {
+          for (const part of step.parts) {
+            if (typeof part !== "string" && part.type == "quest") {
+              const { questGems, vendorGems } = findGems(
+                part,
+                buildData,
+                routeGems
+              );
 
-							for (const gemIndex of questGems) {
-								const requiredGem = buildData.requiredGems[gemIndex];
-								taskItems.push({
-									key: requiredGem.uid,
-									isCompletedState: gemProgressAtomFamily(requiredGem.uid),
-									children: (
-										<GemReward
-											key={taskItems.length}
-											requiredGem={buildData.requiredGems[gemIndex]}
-											type='quest'
-										/>
-									),
-								});
-							}
+              for (const gemIndex of questGems) {
+                const requiredGem = buildData.requiredGems[gemIndex];
+                taskItems.push({
+                  key: requiredGem.uid,
+                  isCompletedState: gemProgressAtomFamily(requiredGem.uid),
+                  children: (
+                    <GemReward
+                      key={taskItems.length}
+                      requiredGem={buildData.requiredGems[gemIndex]}
+                      type="quest"
+                    />
+                  ),
+                });
+              }
 
-							for (const gemIndex of vendorGems) {
-								const requiredGem = buildData.requiredGems[gemIndex];
-								taskItems.push({
-									key: requiredGem.uid,
-									isCompletedState: gemProgressAtomFamily(requiredGem.uid),
-									children: (
-										<GemReward
-											key={taskItems.length}
-											requiredGem={buildData.requiredGems[gemIndex]}
-											type='vendor'
-										/>
-									),
-								});
-							}
-						}
-					}
-				}
-			}
-		}
+              for (const gemIndex of vendorGems) {
+                const requiredGem = buildData.requiredGems[gemIndex];
+                taskItems.push({
+                  key: requiredGem.uid,
+                  isCompletedState: gemProgressAtomFamily(requiredGem.uid),
+                  children: (
+                    <GemReward
+                      key={taskItems.length}
+                      requiredGem={buildData.requiredGems[gemIndex]}
+                      type="vendor"
+                    />
+                  ),
+                });
+              }
+            }
+          }
+        }
+      }
+    }
 
-		const act: number = routeIndex + 1;
-		items.push(<TaskList key={items.length + 2} items={taskItems} act={act} />);
-	}
-	return <>{items}</>;
+    const act = routeIndex + 1;
+    items.push(<ActHolder key={act} act={act} items={taskItems} />);
+  }
+  return <>{items}</>;
+}
+
+interface ActHolderProps {
+  act: number;
+  items: TaskItemProps[];
+}
+
+export function ActHolder({ act, items: taskItems }: ActHolderProps) {
+  const [expanded, setExpanded] = useState(true);
+
+  return (
+    <>
+      <div
+        id={`act-${act}`}
+        className="header"
+        onClick={() => setExpanded(!expanded)}
+      >{`--== Act ${act} ==--`}</div>
+      <hr />
+      {expanded && (
+        <>
+          <TaskList items={taskItems} />
+          <hr />
+        </>
+      )}
+    </>
+  );
 }
 
 export default withScrollRestoration(RoutesContainer);
