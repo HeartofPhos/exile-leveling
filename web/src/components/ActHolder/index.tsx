27ef746eb34a04022b76972d0e314902a2e628c0
heart <>
2022-08-04 00:35:07 +0200

    header expand icons

--- web/src/components/ActHolder/index.tsx
+++ web/src/components/ActHolder/index.tsx
@@ -1,5 +1,8 @@
+import classNames from "classnames";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
 import { atomFamily, useRecoilState } from "recoil";
 import { TaskItemProps, TaskList } from "../TaskList";
+import styles from "./ActHolder.module.css";
 
 const actHolderState = atomFamily<boolean, number>({
   key: "actHolderState",
@@ -14,13 +17,18 @@ interface ActHolderProps {
 export function ActHolder({ act, items: taskItems }: ActHolderProps) {
   const [expanded, setExpanded] = useRecoilState(actHolderState(act));
 
+  const expandIcon = expanded ? <FiChevronUp /> : <FiChevronDown />;
   return (
     <>
       <div
         id={`act-${act}`}
-        className="header"
+        className={classNames("header", styles.row)}
         onClick={() => setExpanded(!expanded)}
-      >{`--== Act ${act} ==--`}</div>
+      >
+        {expandIcon}
+        <div>{`--== Act ${act} ==--`}</div>
+        {expandIcon}
+      </div>
       <hr />
       {expanded && (
         <>
