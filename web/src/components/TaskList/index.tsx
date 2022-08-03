1bf4e8b1c95bc1135121b6a79f086121b4589fca
Joel Chantrell <joelchantrell@protonmail.com>
2022-08-03 20:35:54 +0930

    add functionality to click/drag task items

--- web/src/components/TaskList/index.tsx
+++ web/src/components/TaskList/index.tsx
@@ -9,17 +9,28 @@ export interface TaskItemProps {
 	key?: any;
 	children?: React.ReactNode;
 	isCompletedState?: RecoilState<boolean>;
+	mouseDown?: boolean;
+	setMouseDown?: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-function TaskListItem({ children, isCompletedState }: TaskItemProps) {
+function TaskListItem({
+	children,
+	isCompletedState,
+	mouseDown,
+}: TaskItemProps) {
 	const [isCompleted, setIsCompleted] = isCompletedState
 		? useRecoilState(isCompletedState)
 		: [undefined, undefined];
 
 	return (
 		<li
-			onClick={() => {
+			onMouseDown={() => {
 				if (setIsCompleted) setIsCompleted(!isCompleted);
+			}}
+			onMouseEnter={() => {
+				if (mouseDown) {
+					if (setIsCompleted) setIsCompleted(!isCompleted);
+				}
 			}}
 			className={classNames({ [styles.completed]: isCompleted })}
 		>
@@ -38,30 +49,41 @@ export function TaskList({ items, act }: TaskListProps) {
 		setCollapsed(!collapsed);
 	};
 	const [collapsed, setCollapsed] = useState(false);
+	const [mouseDown, setMouseDown] = useState(false);
 	if (items) {
 		return (
 			<>
 				<div
-					id={`act-${act}`}
-					key={items.length}
-					className='header'
-					onClick={() => collapseSection()}
-				>{`--== Act ${act} ==--`}</div>
-				{!collapsed ? (
-					<>
-						<hr key={items.length + 1} />
-						<ol className={classNames(styles.list)}>
-							{items &&
-								items.map(({ key, ...rest }, i) => (
-									<TaskListItem key={key || i} {...rest} />
-								))}
-						</ol>
-					</>
-				) : (
-					<></>
-				)}
-				<hr key={items.length + 3} />
-				<div />
+					onMouseDown={() => setMouseDown(true)}
+					onMouseUp={() => setMouseDown(false)}
+				>
+					<div
+						id={`act-${act}`}
+						key={items.length}
+						className='header'
+						onClick={() => collapseSection()}
+					>{`--== Act ${act} ==--`}</div>
+					{!collapsed ? (
+						<>
+							<hr key={items.length + 1} />
+							<ol className={classNames(styles.list)}>
+								{items &&
+									items.map(({ key, ...rest }, i) => (
+										<TaskListItem
+											key={key || i}
+											{...rest}
+											mouseDown={mouseDown}
+											setMouseDown={setMouseDown}
+										/>
+									))}
+							</ol>
+						</>
+					) : (
+						<></>
+					)}
+					<hr key={items.length + 3} />
+					<div />
+				</div>
 			</>
 		);
 	} else return <></>;
