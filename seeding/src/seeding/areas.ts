93cb6b1fda37a1491e7ab107bdad982f77725c22
Heart <>
2022-08-28 12:30:30 +0200

    update data

--- seeding/src/seeding/areas.ts
+++ seeding/src/seeding/areas.ts
@@ -1,15 +1,25 @@
 import { Areas } from "../../../common/types";
 import { RecipeUnlockDisplayDat, WorldAreasDat } from "../../data";
 
+const seedAreaIds = [
+  // The Twilight Strand
+  "1_1_1",
+  // Aspirant's Trial
+  "1_Labyrinth_boss_3",
+  // Cruel Aspirant's Trial
+  "2_Labyrinth_boss_3",
+  // Merciless Aspirant's Trial
+  "3_Labyrinth_boss_3",
+];
+
 export async function getAreas() {
   const result: Areas = {};
 
-  // The Twilight Strand
-  // Aspirant's Trial
-  // Cruel Aspirant's Trial
-  // Merciless Aspirant's Trial
-  let todo = [2, 519, 591, 663];
-  let done = new Set<number>();
+  const todo = WorldAreasDat.data.reduce<number[]>((p, c, i) => {
+    if (seedAreaIds.some((areaId) => areaId == c.Id)) p.push(i);
+    return p;
+  }, []);
+  const done = new Set<number>();
 
   while (todo.length > 0) {
     const worldAreasKey = todo.shift()!;
