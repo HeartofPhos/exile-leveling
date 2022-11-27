da16ab0d3114d62d779317211fa284829f1dbe99
Heart <>
2022-11-27 20:31:31 +0200

    sanity

--- web/src/components/BuildImportForm/index.tsx
+++ web/src/components/BuildImportForm/index.tsx
@@ -14,7 +14,7 @@ type UrlImporter = (url: string) => string | null;
 
 const urlImporters: UrlImporter[] = [
   (url) => {
-    const match = /pastebin\.com\/(\w+)\s*$/.exec(url);
+    const match = /pastebin\.com\/(\w+)/.exec(url);
     if (!match) return null;
 
     return `https://pastebin.com/raw/${match[1]}`;
