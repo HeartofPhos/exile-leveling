7b430cbc4f7eee6892fc780c567619d0466d38e1
Heart <>
2022-11-27 12:44:36 +0200

    sidebar styling

--- web/src/components/Sidebar/index.tsx
+++ web/src/components/Sidebar/index.tsx
@@ -1,5 +1,7 @@
 import classNames from "classnames";
+import { useState } from "react";
 import { FaRegClipboard } from "react-icons/fa";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import { useRecoilValue } from "recoil";
 import { buildDataSelector } from "../../utility/state/build-data-state";
 
@@ -7,23 +9,53 @@ import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
   const buildData = useRecoilValue(buildDataSelector);
+  const [expanded, setExpanded] = useState(true);
 
   if (buildData == null || buildData.vendorStrings.length == 0) return <></>;
 
+  const expandIcon = expanded ? <FiChevronRight /> : <FiChevronLeft />;
+
   return (
-    <div className={classNames(styles.sidebar)}>
-      {buildData.vendorStrings.map((x) => (
-        <>
-          <span
-            onClick={() => {
-              navigator.clipboard.writeText(x);
-            }}
-          >
-            <FaRegClipboard className={classNames("inlineIcon")} /> {x}
-          </span>
-          <hr />
-        </>
-      ))}
+    <div
+      className={classNames(styles.sidebar, {
+        [styles.expand]: expanded,
+      })}
+    >
+      <div
+        className={classNames(styles.sidebarToggle)}
+        onClick={() => {
+          setExpanded(!expanded);
+        }}
+      >
+        {expanded && <span>Vendor Strings</span>}
+        {expandIcon}
+      </div>
+      {expanded && (
+        <div className={classNames(styles.sidebarItems)}>
+          {buildData.vendorStrings.map((x) => (
+            <VendorString value={x} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+interface VendorStringProps {
+  value: string;
+}
+function VendorString({ value }: VendorStringProps) {
+  return (
+    <div
+      className={classNames(styles.vendorString)}
+      onClick={() => {
+        navigator.clipboard.writeText(value);
+      }}
+    >
+      <div>
+        <FaRegClipboard className={classNames("inlineIcon")} />
+      </div>
+      {value}
     </div>
   );
 }
