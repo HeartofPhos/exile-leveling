6dd5d47cb55c2d987b60245217f2446ad0506979
Heart <>
2022-12-07 22:38:25 +0200

    update pwa icons

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -10,24 +10,26 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       manifest: {
-        name: 'Exile Leveling',
-        short_name: 'Exile Leveling',
-        description: 'Exile Leveling',
-        theme_color: '#000000',
+        name: "Exile Leveling",
+        short_name: "Exile Leveling",
+        description:
+          "Exile Leveling is a Path of Exile leveling guide with Path of Building integration",
+        theme_color: "#000000",
         icons: [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      }
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
     }),
   ],
 });
