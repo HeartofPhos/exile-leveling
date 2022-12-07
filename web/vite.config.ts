0c49218eb8aa0706b13194d1a405e0907a247afe
Thomas Marcelin <t.marcelin@betclicgroup.com>
2022-12-07 13:07:49 +0100

    feat: add PWA compatibility

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/exile-leveling/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: 'Exile Leveling',
+        short_name: 'Exile Leveling',
+        description: 'Exile Leveling',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    }),
+  ],
 });
