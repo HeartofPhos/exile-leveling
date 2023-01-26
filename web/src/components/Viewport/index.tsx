8a87b16be791797a3ad1c47be473fcff876d50ce
Heart <>
2023-01-26 22:12:39 +0200

    input area

--- web/src/components/Viewport/index.tsx
+++ web/src/components/Viewport/index.tsx
@@ -1,16 +1,18 @@
 import classNames from "classnames";
 import { useEffect, useRef, useState } from "react";
-
 import styles from "./styles.module.css";
 
 export function Viewport({ children }: React.PropsWithChildren) {
   const divRef = useRef<HTMLDivElement>(null);
   const [pos, setPos] = useState({ x: 0, y: 0 });
   const [scale, setScale] = useState(1);
+  const [inputActive, setInputActive] = useState(false);
 
   useEffect(() => {
     if (divRef.current === null) return;
-    divRef.current.addEventListener("pointerdown", (evt) => evt.preventDefault());
+    divRef.current.addEventListener("pointerdown", (evt) =>
+      evt.preventDefault()
+    );
     divRef.current.addEventListener("wheel", (evt) => evt.preventDefault());
     divRef.current.addEventListener("drag", (evt) => evt.preventDefault());
   }, [divRef]);
@@ -19,8 +21,16 @@ export function Viewport({ children }: React.PropsWithChildren) {
     <div
       ref={divRef}
       className={classNames(styles.viewport)}
+      onPointerDown={(evt) => {
+        if (!evt.isPrimary) return;
+        setInputActive(true);
+      }}
+      onPointerUp={(evt) => {
+        if (!evt.isPrimary) return;
+        setInputActive(false);
+      }}
       onPointerMove={(evt) => {
-        if ((evt.buttons & 1) !== 1) return;
+        if (evt.pointerType === "mouse" && (evt.buttons & 1) !== 1) return;
 
         setPos({
           x: pos.x + evt.movementX,
@@ -46,6 +56,11 @@ export function Viewport({ children }: React.PropsWithChildren) {
         });
       }}
     >
+      <div
+        className={classNames(styles.inputArea, {
+          [styles.active]: inputActive,
+        })}
+      />
       <div
         style={{
           transformOrigin: "0 0",
