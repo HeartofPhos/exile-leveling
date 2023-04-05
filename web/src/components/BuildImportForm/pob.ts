9ef15fee77a8ccd77bd42134b58f08c41e9bb925
heart <heart>
2023-04-05 11:59:46 +0200

    fix build

--- web/src/components/BuildImportForm/pob.ts
+++ web/src/components/BuildImportForm/pob.ts
@@ -136,6 +136,7 @@ export function processPob(pobCode: string): PobData | undefined {
       bandit: bandit as RouteData.BuildData["bandit"],
       leagueStart: true,
       library: true,
+      gemsOnly: false,
     },
     requiredGems,
     buildTrees,
