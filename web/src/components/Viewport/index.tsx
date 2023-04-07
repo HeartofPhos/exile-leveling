05f59877fdfcb67f6ef5044c27bfedbc097fd278
Dylan Bowers <dylanvolibowers@gmail.com>
2023-04-07 00:06:53 -0700

    change matrix3d transform into two simpler transforms

--- web/src/components/Viewport/index.tsx
+++ web/src/components/Viewport/index.tsx
@@ -66,9 +66,10 @@ export function Viewport({
     const zoom = d3
       .zoom<HTMLDivElement, unknown>()
       .on("zoom", ({ transform }) => {
+        console.log(transform)
         worldSelection.style(
           "transform",
-          `matrix3d(${transform.k}, 0, 0, 0, 0, ${transform.k}, 0, 0, 0, 0, 1, 0, ${transform.x}, ${transform.y}, 0, 1)`
+          `translate(${transform.x}px, ${transform.y}px) scale(${transform.k})`
         );
       });
 
