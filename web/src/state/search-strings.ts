bbcedb515373d7a9c1baee99d18cee7738840ccf
Heart <>
2023-08-14 22:29:56 +0530

    sanity

--- web/src/state/search-strings.ts
+++ web/src/state/search-strings.ts
@@ -4,13 +4,8 @@ import { atom } from "recoil";
 
 const SEARCH_STRINGS_VERSION = 0;
 
-const MIGRATORS = BuildMigratorMap([
-  [0, 1, (old: string[]) => old.map((x) => "#" + x)],
-  [1, 2, (old: string[]) => old.map((x) => "!" + x)],
-]);
-
 export const searchStringsAtom = atom<string[] | null>({
   key: "searchStringsAtom",
-  default: getPersistent("search-strings", SEARCH_STRINGS_VERSION, MIGRATORS),
+  default: getPersistent("search-strings", SEARCH_STRINGS_VERSION, {}),
   effects: [persistentStorageEffect("search-strings", SEARCH_STRINGS_VERSION)],
 });
