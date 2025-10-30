1c6bf79495cd4aa30be9d8d042c08c45d012b4a0
Heart <>
2025-10-31 01:56:30 +0200

    remove telemetry

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,12 +1,9 @@
 import { App } from "./containers";
 import "./index.css";
-import { loadAnalytics } from "./utility/telemetry";
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
-
-loadAnalytics();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- web/src/utility/telemetry.ts
+++ web/src/utility/telemetry.ts
@@ -1,29 +1,6 @@
-import { ReactPlugin } from "@microsoft/applicationinsights-react-js";
-import { ApplicationInsights } from "@microsoft/applicationinsights-web";
-import { createHashHistory } from "history";
-
-const history = createHashHistory();
-const reactPlugin = new ReactPlugin();
-const appInsights = new ApplicationInsights({
-  config: {
-    connectionString: import.meta.env.VITE_ANALYTICS_KEY,
-    enableAutoRouteTracking: true,
-    extensions: [reactPlugin],
-    extensionConfig: {
-      [reactPlugin.identifier]: { history },
-    },
-  },
-});
-
-export function loadAnalytics() {
-  if (import.meta.env.VITE_ANALYTICS_KEY) {
-    appInsights.loadAppInsights();
-  }
-}
-
 export function trackEvent(event: {
   name: string;
   properties?: Record<string, any>;
 }) {
-  reactPlugin.trackEvent(event);
+  // TODO figure out telemetry
 }
